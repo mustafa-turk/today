eaeed2b7d8ac9818de089597de2fb01fde5a24e2
Mustafa Türk <43481284+mustafa-turk@users.noreply.github.com>
2024-02-01 04:59:28 +0100

    move launcher and notification logic to hooks

--- App.tsx
+++ App.tsx
@@ -1,14 +1,8 @@
 import "react-native-gesture-handler";
 
-import * as React from "react";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-
-import * as Device from "expo-device";
-import * as Notifications from "expo-notifications";
 
 import HomeScreen from "@/screens/home";
 import EventDetails from "@/screens/event-details";
@@ -16,64 +10,19 @@ import NewCalendar from "@/screens/new-calendar";
 import GrantAccessScreen from "@/screens/get-started/grant-access";
 import GetStartedScreen from "@/screens/get-started/get-started";
 
+import useNotifications from "@/hooks/useNotifications";
+import useLauncher from "@/hooks/useLauncher";
+
 import { RootStackParamList } from "@/utils/types";
-import { SCREENS, STORAGE } from "@/utils/constants";
+import { SCREENS } from "@/utils/constants";
 
 import "@/utils/i18n";
 
 const Stack = createStackNavigator<RootStackParamList>();
 
-Notifications.setNotificationHandler({
-  handleNotification: async () => ({
-    shouldShowAlert: true,
-    shouldPlaySound: false,
-    shouldSetBadge: false,
-  }),
-});
-
 export default function App() {
-  const [launchRouteName, setLaunchRouteName] = React.useState(null);
-
-  const [expoPushToken, setExpoPushToken] = React.useState("");
-  const [notification, setNotification] =
-    React.useState<Notifications.Notification>(null);
-  const notificationListener = React.useRef();
-  const responseListener = React.useRef();
-
-  React.useEffect(() => {
-    registerForPushNotificationsAsync().then((token) =>
-      setExpoPushToken(token)
-    );
-
-    notificationListener.current =
-      Notifications.addNotificationReceivedListener((notification) => {
-        setNotification(notification);
-      });
-
-    responseListener.current =
-      Notifications.addNotificationResponseReceivedListener((response) => {
-        console.log(response);
-      });
-
-    return () => {
-      Notifications.removeNotificationSubscription(
-        notificationListener.current
-      );
-      Notifications.removeNotificationSubscription(responseListener.current);
-    };
-  }, []);
-
-  React.useEffect(() => {
-    (async () => {
-      const hasLaunched = await AsyncStorage.getItem(STORAGE.HAS_LAUNCHED);
-      if (hasLaunched) {
-        setLaunchRouteName(SCREENS.HOME);
-      } else {
-        await AsyncStorage.setItem(STORAGE.HAS_LAUNCHED, "1");
-        setLaunchRouteName(SCREENS.GET_STARTED);
-      }
-    })();
-  }, []);
+  const { launchRouteName } = useLauncher();
+  useNotifications();
 
   return (
     <>
@@ -112,25 +61,4 @@ export default function App() {
       )}
     </>
   );
-}
-
-async function registerForPushNotificationsAsync() {
-  let token: string;
-
-  if (Device.isDevice) {
-    const { status: existingStatus } =
-      await Notifications.getPermissionsAsync();
-    let finalStatus = existingStatus;
-    if (existingStatus !== "granted") {
-      const { status } = await Notifications.requestPermissionsAsync();
-      finalStatus = status;
-    }
-    if (finalStatus !== "granted") {
-      alert("Failed to get push token for push notification!");
-      return;
-    }
-    token = (await Notifications.getExpoPushTokenAsync()).data;
-  }
-
-  return token;
 }
---
+++ src/hooks/useNotifications.ts
@@ -0,0 +1,63 @@
+import * as React from "react";
+import * as Notifications from "expo-notifications";
+import * as Device from "expo-device";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
+export default function useNotification() {
+  const [expoPushToken, setExpoPushToken] = React.useState("");
+  const [notification, setNotification] =
+    React.useState<Notifications.Notification>(null);
+  const notificationListener = React.useRef();
+  const responseListener = React.useRef();
+
+  React.useEffect(() => {
+    registerForPushNotificationsAsync().then((token) =>
+      setExpoPushToken(token)
+    );
+
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
+
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    return () => {
+      Notifications.removeNotificationSubscription(
+        notificationListener.current
+      );
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+}
+
+async function registerForPushNotificationsAsync() {
+  let token: string;
+
+  if (Device.isDevice) {
+    const { status: existingStatus } =
+      await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== "granted") {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== "granted") {
+      alert("Failed to get push token for push notification!");
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+  }
+
+  return token;
+}
